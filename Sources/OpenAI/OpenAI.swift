02a9cd8b9f9ac9c1f454c857387c0b96e17abeb6
Reto Schneider <reto.schneider@outlook.com>
2023-04-15 15:29:05 +0200

    bugfix new destination url

--- Sources/OpenAI/OpenAI.swift
+++ Sources/OpenAI/OpenAI.swift
@@ -1,42 +1,7 @@
 import Foundation
 import Alamofire
 
-public class OpenAI {
-    private let apiKey: String
-    private let apiUrl = "https://api.openai.com/v1/engines/davinci-codex/completions"
-
-    public init(apiKey: String) {
-        self.apiKey = apiKey
-    }
-
-    public func complete(prompt: String, maxTokens: Int, completion: @escaping (Result<CompletionResult, Error>) -> Void) {
-        let headers: HTTPHeaders = [
-            "Content-Type": "application/json",
-            "Authorization": "Bearer \(apiKey)"
-        ]
-        
-        let parameters: [String: Any] = [
-            "prompt": prompt,
-            "max_tokens": maxTokens,
-            "n": 1,
-            "stop": ["\n"]
-        ]
-
-        AF.request(apiUrl, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
-            .validate()
-            .responseDecodable(of: CompletionResponse.self) { response in
-                switch response.result {
-                    case .success(let completionResponse):
-                        let completionResult = CompletionResult(choices: completionResponse.choices)
-                        completion(.success(completionResult))
-                    case .failure(let error):
-                        completion(.failure(error))
-                }
-            }
-    }
-}
-
-public struct CompletionResponse: Decodable {
+public struct CompletionResponse: Codable {
     public let id: String
     public let object: String
     public let created: Int
@@ -44,20 +9,59 @@ public struct CompletionResponse: Decodable {
     public let usage: Usage
     public let choices: [Choice]
 
-    public struct Usage: Decodable {
+    public struct Usage: Codable {
         public let promptTokens: Int
         public let completionTokens: Int
         public let totalTokens: Int
     }
 
-    public struct Choice: Decodable {
+    public struct Choice: Codable {
         public let text: String
         public let index: Int
-        public let logprobs: String?
+        public let logprobs: LogProbs?
         public let finishReason: String
+
+        public struct LogProbs: Codable {
+            public let tokens: [String]
+            public let tokenLogprobs: [Double]
+            public let topLogprobs: [[String: Double]]
+            public let textOffset: [Int]
+        }
     }
 }
 
-public struct CompletionResult {
-    public let choices: [CompletionResponse.Choice]
+struct CompletionRequest: Codable {
+    let model: String
+    let prompt: String
+    let maxTokens: Int
+    let temperature: Double
+}
+
+public class OpenAI {
+    private let apiKey: String
+    private let baseURL = "https://api.openai.com/v1/completions"
+
+    public init(apiKey: String) {
+        self.apiKey = apiKey
+    }
+
+    public func complete(prompt: String, maxTokens: Int, temperature: Double, completion: @escaping (Result<CompletionResponse, Error>) -> Void) {
+        let requestPayload = CompletionRequest(model: "text-davinci-003", prompt: prompt, maxTokens: maxTokens, temperature: temperature)
+        
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer \(apiKey)"
+        ]
+
+        AF.request(baseURL, method: .post, parameters: requestPayload, encoder: JSONParameterEncoder.default, headers: headers)
+            .validate()
+            .responseDecodable(of: CompletionResponse.self) { response in
+                switch response.result {
+                case .success(let completionResult):
+                    completion(.success(completionResult))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
 }
